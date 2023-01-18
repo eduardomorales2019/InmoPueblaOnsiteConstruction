357e76a822944a28cf4818b25e73e6c977a0aeb3
EduardoMorales <eduardo@inmo-puebla.com>
2023-01-17 21:52:10 -0600

    se agrego la A y la m

--- src/App.js
+++ src/App.js
@@ -1,19 +1,19 @@
-import "./App.css";
+import "./App.css"
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <div className="hero">
-          <h2 className="sitio">Sitio en construcción</h2>
-          <h1> Inmo - Puebla</h1>
+    <div className='App'>
+      <header className='App-header'>
+        <div className='hero'>
+          <h2 className='sitio'>Sitio en construcción</h2>
+          <h1> Inmo - PueblA</h1>
           {/* <h3 className="naming">
             compra, renta , administracion de propiedades
           </h3> */}
         </div>
       </header>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
